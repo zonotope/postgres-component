d099612d4ab5447bcbfa0b6013adc37e16bb4f3b
ben lamothe <zonotope@gmail.com>
2016-07-12 21:38:49 -0400

    add dependencies and core functionality

--- src/postgres_component/core.clj
+++ src/postgres_component/core.clj
@@ -1,6 +1,57 @@
-(ns postgres-component.core)
+(ns postgres-component.core
+  (:require [hikari-cp.core :refer [make-datasource close-datasource]]
+            [clojure.java.jdbc :as jdbc]
+            [com.stuartsierra.component :as component]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn- make-spec [postgres]
+  {:datasource (make-datasource postgres)})
+
+(defn- default-db-spec
+  "Returns the specification of the default ('postgres') database"
+  [{:keys [username password server-name] :as config}]
+  (let [subname (str "//" server-name "/postgres")]
+    {:subprotocol "postgresql"
+     :subname subname
+     :user username
+     :password password}))
+
+(defn- vectorize-string [s]
+  (if (string? s) [s] s))
+
+(defn- execute-default!
+  "Execute statement on the default ('postgres') database"
+  ([postgres statement]
+   (execute-default! postgres statement {}))
+
+  ([postgres statement {:keys [transaction?] :or {transaction? true} :as opts}]
+   (let [spec (default-db-spec postgres)
+         statement (vectorize-string statement)]
+    (jdbc/with-db-connection [conn spec]
+      (jdbc/db-do-commands conn transaction? statement)))))
+
+(defrecord Postgres []
+  component/Lifecycle
+  (start [postgres]
+    (if (:spec postgres)
+      postgres
+      (assoc postgres
+             :spec (make-spec postgres))))
+
+  (stop [postgres]
+    (if-let [datasource (-> postgres :spec :datasource)]
+      (do (close-datasource datasource)
+          (dissoc postgres :spec))
+      postgres)))
+
+(defn postgres [config]
+  (-> config
+      (assoc :adapter "postgresql")
+      (map->Postgres)))
+
+(defn create-database! [{:keys [database-name] :as postgres}]
+  (execute-default! postgres (str "CREATE DATABASE " database-name)
+                    {:transaction? false}))
+
+(defn drop-database! [{:keys [database-name] :as postgres}]
+  (execute-default! postgres (str "DROP DATABASE " database-name)
+                    {:transaction? false}))
